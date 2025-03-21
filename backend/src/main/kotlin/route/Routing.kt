9dc8612fdf4f5c7ced04fe04105616cd9564380f
akrck02 <aketza.vazquez@gmail.com>
2025-03-21 19:10:13 +0100

    kotlin: ktor template

---
+++ backend/src/main/kotlin/route/Routing.kt
@@ -0,0 +1,13 @@
+package org.akrck02.route
+
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
