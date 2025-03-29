30a530a2e916a6fdd3a4abf1d3fac6d72005d03b
akrck02 <aketza.vazquez@gmail.com>
2025-03-29 18:40:51 +0100

    view: lock

--- Countless/composeApp/src/androidMain/kotlin/org/akrck02/countless/App.kt
+++ Countless/composeApp/src/androidMain/kotlin/org/akrck02/countless/App.kt
@@ -2,47 +2,28 @@ package org.akrck02.countless
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Fingerprint
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.em
 import org.akrck02.countless.system.biometric.authenticateWithBiometrics
-import org.akrck02.countless.ui.extension.modify
 import org.akrck02.countless.ui.menu.BottomNavigationBar
 import org.akrck02.countless.ui.theme.getSystemThemeColors
+import org.akrck02.countless.ui.view.LockView
 import org.akrck02.countless.viewmodel.AppViewModel
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.androidx.compose.koinViewModel
-import java.time.LocalDate
 
 @RequiresApi(Build.VERSION_CODES.R)
 @Composable
 @Preview
 fun App(appViewModel: AppViewModel = koinViewModel()) {
 
-    var authenticated by remember { mutableStateOf(true) }
-
+    var authenticated by remember { mutableStateOf(false) }
     MaterialTheme(
         colorScheme = getSystemThemeColors(),
         shapes = MaterialTheme.shapes
@@ -53,66 +34,15 @@ fun App(appViewModel: AppViewModel = koinViewModel()) {
 
             contentPadding.toString()
 
+            var context = LocalContext.current
             if (authenticated.not()) {
-
-                Column(
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier.fillMaxSize()
-                ) {
-                    Text(
-                        text = "Countless",
-                        fontSize = 12.5.em,
-                        fontWeight = FontWeight.Light,
-                        color = MaterialTheme.colorScheme.onSurface,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(bottom = 10.dp)
+                LockView {
+                    authenticateWithBiometrics(
+                        context = context,
+                        onError = { authenticated = false },
+                        onSuccess = { authenticated = true }
                     )
-
-                    Text(
-                        text = "Please enter your fingerprint to access.",
-                        fontSize = 4.em,
-                        fontWeight = FontWeight.Light,
-                        color = MaterialTheme.colorScheme.onSurface.modify(.5f),
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(bottom = 300.dp)
-                    )
-
-                    var context = LocalContext.current
-                    Surface(
-                        onClick = {
-                            authenticateWithBiometrics(
-                                context = context,
-                                onError = { authenticated = false },
-                                onSuccess = { authenticated = true }
-                            )
-                        }
-                    ) {
-                        Icon(
-                            imageVector = Icons.Rounded.Fingerprint,
-                            tint = MaterialTheme.colorScheme.primary,
-                            contentDescription = "",
-                            modifier = Modifier.size(70.dp)
-                        )
-                    }
-
-                    Text(
-                        text = "Akrck02 - ${LocalDate.now().year}",
-                        fontSize = 4.em,
-                        fontWeight = FontWeight.Light,
-                        color = MaterialTheme.colorScheme.onSurface.modify(.5f),
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 30.dp)
-                    )
-
                 }
-
 
                 return@Scaffold
             }
---
+++ Countless/composeApp/src/androidMain/kotlin/org/akrck02/countless/ui/view/LockView.kt
@@ -0,0 +1,81 @@
+package org.akrck02.countless.ui.view
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Fingerprint
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import org.akrck02.countless.ui.extension.modify
+import java.time.LocalDate
+
+@Composable
+@RequiresApi(Build.VERSION_CODES.O)
+fun LockView(onUnlockClick: () -> Unit) {
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = "Countless",
+            fontSize = 12.5.em,
+            fontWeight = FontWeight.Light,
+            color = MaterialTheme.colorScheme.onSurface,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 10.dp)
+        )
+
+        Text(
+            text = "Please enter your fingerprint to access.",
+            fontSize = 4.em,
+            fontWeight = FontWeight.Light,
+            color = MaterialTheme.colorScheme.onSurface.modify(.5f),
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 300.dp)
+        )
+
+        Surface(
+            onClick = onUnlockClick
+        ) {
+            Icon(
+                imageVector = Icons.Rounded.Fingerprint,
+                tint = MaterialTheme.colorScheme.primary,
+                contentDescription = "",
+                modifier = Modifier.size(70.dp)
+            )
+        }
+
+        Text(
+            text = "Akrck02 - ${LocalDate.now().year}",
+            fontSize = 4.em,
+            fontWeight = FontWeight.Light,
+            color = MaterialTheme.colorScheme.onSurface.modify(.5f),
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 30.dp)
+        )
+
+    }
+}
